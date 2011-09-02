bc929d73bdf1506ac90692fb149a14a7c5a0226a
Mei Dahua <tatwah.mui@gmail.com>
2011-09-02 14:09:17 +0800

    remove non-exist mem bank which cause mm_init() crash

--- arch/arm/mach-msm/board-buzz.c
+++ arch/arm/mach-msm/board-buzz.c
@@ -1241,9 +1241,11 @@ static void __init buzz_fixup(struct machine_desc *desc, struct tag *tags,
 	mi->bank[0].start = MSM_LINUX_BASE1;
 	mi->bank[0].node = PHYS_TO_NID(MSM_LINUX_BASE1);
 	mi->bank[0].size = MSM_LINUX_SIZE1;
+/*  
 	mi->bank[1].start = MSM_LINUX_BASE2;
 	mi->bank[1].node = PHYS_TO_NID(MSM_LINUX_BASE2);
 	mi->bank[1].size = MSM_LINUX_SIZE2;
+*/
 }
 
 static void __init buzz_map_io(void)
